318aa47af086b1541b894c53ee18889026dd675e
MatthieuJ <matthieu.jasselette@gmail.com>
2019-08-28 15:16:02 +0200

    add token validity control

--- src/main.js
+++ src/main.js
@@ -2,11 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  created() {
+    axios.interceptors.response.use(
+      response => response,
+      error => {
+        if (error.response.status === 401) {
+          this.$store.dispatch('logout')
+        }
+        return Promise.reject(error)
+      }
+    )
+  },
   render: h => h(App)
 }).$mount('#app')
