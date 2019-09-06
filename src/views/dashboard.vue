32351a7ede200152477af9991f0264e2546f48bf
MatthieuJ <matthieu.jasselette@gmail.com>
2019-09-06 13:16:25 +0200

    add relevant store getters & dispatch

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
-
-  </div>
+  <div>Restricted to admin</div>
 </template>
 
 <script>
-  export default {
-    
-  }
+import { mapState } from 'vuex'
+
+export default {
+  created() {
+    this.$store.dispatch('fetchDashboardUsers'),
+      this.$store.dispatch('fetchDashboardCharacters'),
+      this.$store.dispatch('fetchDashboardSnapshots')
+  },
+  computed: mapState({
+    users: state => state.dashboard.users,
+    characters: state => state.dashboard.characters,
+    snapshots: state => state.dashboard.snapshots
+  })
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
