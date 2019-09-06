39cf03d4901129e2f60d26fb653b0838f433c07c
MatthieuJ <matthieu.jasselette@gmail.com>
2019-09-06 14:26:46 +0200

    props relevant data to child, add DashboardList as child

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -1,11 +1,22 @@
 <template>
-  <div>Restricted to admin</div>
+  <div>
+    Admin Dashboard
+    <DashboardList
+      :users="users"
+      :characters="characters"
+      :snapshots="snapshots"
+    />
+  </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import DashboardList from '@/components/DashboardList.vue'
 
 export default {
+  components: {
+    DashboardList
+  },
   created() {
     this.$store.dispatch('fetchDashboardUsers'),
       this.$store.dispatch('fetchDashboardCharacters'),
