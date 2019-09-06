4706c199fe012b3cbf5d98371659501c0430fef5
MatthieuJ <matthieu.jasselette@gmail.com>
2019-09-06 15:19:47 +0200

    extract raid mutation & action to a separate module

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import ApiService from '@/services/ApiService.js'
 import * as users from '@/store/modules/users.js'
 import * as characters from '@/store/modules/characters.js'
+import * as raids from '@/store/modules/raids.js'
 import * as dashboard from '@/store/modules/dashboard.js'
 import * as notification from '@/store/modules/notifications.js'
 
@@ -12,31 +12,12 @@ export default new Vuex.Store({
   modules: {
     users,
     characters,
+    raids,
     dashboard,
     notification
   },
-  state: {
-    raid: []
-  },
-  mutations: {
-    SET_RAID(state, raid) {
-      state.raid = raid
-    }
-  },
-  actions: {
-    fetchRaid({ commit, dispatch }) {
-      ApiService.getRaid()
-        .then(response => commit('SET_RAID', response.data.data))
-        .catch(error => {
-          const notification = {
-            type: 'error',
-            message: 'There was a problem fetching events: ' + error.message
-          }
-          dispatch('notification/add', notification, {
-            root: true
-          })
-        })
-    }
-  },
+  state: {},
+  mutations: {},
+  actions: {},
   getters: {}
 })
