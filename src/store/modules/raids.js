b72dba28c7807d5178ebdd16da0bfe9729e0628a
MatthieuJ <matthieu.jasselette@gmail.com>
2019-09-06 16:29:58 +0200

    add fetchsnapshot action & mutation

--- src/store/modules/raids.js
+++ src/store/modules/raids.js
@@ -2,19 +2,18 @@ import ApiService from '@/services/ApiService.js'
 
 export const state = {
   raid: [],
-  snapshots: [],
   snapshot: {}
 }
 
-export const getters = {
-  getSnapshotById: state => id => {
-    return state.snapshots.find(snapshot => snapshot.id === id)
-  }
-}
+export const getters = {}
 
 export const mutations = {
   SET_RAID(state, raid) {
     state.raid = raid
+  },
+
+  SET_SNAPSHOT(state, snapshot) {
+    state.snapshot = snapshot
   }
 }
 
@@ -26,6 +25,50 @@ export const actions = {
         const notification = {
           type: 'error',
           message: 'There was a problem fetching events: ' + error.message
+        }
+        dispatch('notification/add', notification, {
+          root: true
+        })
+      })
+  },
+
+  fetchSnapshot({ commit, getters, dispatch }, id) {
+    let snapshot = getters.getSnapshotById(id)
+    if (snapshot) {
+      commit('SET_SNAPSHOT', snapshot)
+    } else {
+      ApiService.getSnapshot(id)
+        .then(response => commit('SET_SNAPSHOT', response.data.data))
+        .catch(error => {
+          const notification = {
+            type: 'error',
+            message:
+              'There was a problem fetching the snapshot: ' + error.message
+          }
+          dispatch('notification/add', notification, {
+            root: true
+          })
+        })
+    }
+  },
+
+  saveSnapshot({ commit, dispatch }) {
+    let snapshot = {
+      snapshot: []
+    }
+    state.raid.map(character => {
+      snapshot.snapshot.push({
+        id: character.id,
+        name: character.name,
+        owner: character.user.name
+      })
+    })
+    ApiService.saveSnapshot(snapshot)
+      .then(response => commit('ADD_SNAPSHOT', response.data))
+      .catch(error => {
+        const notification = {
+          type: 'error',
+          message: 'There was a problem saving the snapshot: ' + error.message
         }
         dispatch('notification/add', notification, {
           root: true
