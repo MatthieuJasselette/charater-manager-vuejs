296921cf4c499e7ae9ef5c4a5c75940a95af4197
MatthieuJ <matthieu.jasselette@gmail.com>
2019-09-06 11:34:28 +0200

    create and flesh out admin dashboard view & store module

---
+++ src/store/modules/dashboard.js
@@ -0,0 +1,63 @@
+import ApiService from '@/services/ApiService.js'
+
+export const state = {
+  users: [],
+  characters: [],
+  snapshots: []
+}
+
+export const getters = {}
+export const mutations = {
+  SET_USERS(state, users) {
+    state.users = users
+  },
+  SET_CHARACTERS(state, characters) {
+    state.characters = characters
+  },
+  SET_SNAPSHOTS(state, snapshots) {
+    state.snapshots = snapshots
+  }
+}
+export const actions = {
+  fetchDashboardUsers({ commit, dispatch }) {
+    ApiService.getDashboardUsers()
+      .then(response => commit('SET_USERS', response.data.data))
+      .catch(error => {
+        const notification = {
+          type: 'error',
+          message: 'There was a problem fetching users: ' + error.message
+        }
+        dispatch('notification/add', notification, {
+          root: true
+        })
+      })
+  },
+
+  fetchDashboardCharacters({ commit, dispatch }) {
+    ApiService.getDashboardCharacters()
+      .then(response => commit('SET_CHARACTERS', response.data.data))
+      .catch(error => {
+        const notification = {
+          type: 'error',
+          message: 'There was a problem fetching characters: ' + error.message
+        }
+        dispatch('notification/add', notification, {
+          root: true
+        })
+      })
+  },
+
+  fetchDashboardSnapshots({ commit, dispatch }) {
+    ApiService.getDashboardSnapshots()
+      .then(response => commit('SET_SNAPSHOTS', response.data.data))
+      .catch(error => {
+        const notification = {
+          type: 'error',
+          message: 'There was a problem fetching SNAPSHOTS: ' + error.message
+        }
+        dispatch('notification/add', notification, {
+          root: true
+        })
+      })
+  }
+}
